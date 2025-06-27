722dbbdfea076cec6e811b6427844c0925f6e0c1
Raufur Islam <raufurislam@gmail.com>
2025-06-28 00:39:18 +0600

    dark theme shadcn

--- redux-bootstrap/src/main.tsx
+++ redux-bootstrap/src/main.tsx
@@ -5,11 +5,14 @@ import { RouterProvider } from "react-router";
 import router from "./routes/index.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
+import { ThemeProvider } from "./providers/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ThemeProvider>
   </StrictMode>
 );
