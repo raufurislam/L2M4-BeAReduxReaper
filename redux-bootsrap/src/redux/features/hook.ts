e22e2d39f52c708154784edf71a392cfc25d860e
Raufur Islam <raufurislam@gmail.com>
2025-06-27 18:17:03 +0600

    Shadcn init on project

--- redux-bootsrap/src/redux/features/hook.ts
+++
@@ -1,6 +0,0 @@
-// hook.ts
-import { useDispatch, useSelector } from "react-redux";
-import type { AppDispatch, RootSate } from "../store";
-
-export const useAppSelector = useSelector.withTypes<RootSate>();
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
