3f792783a12e518978a2c204156dabe8cb28c86c
Raufur Islam <raufurislam@gmail.com>
2025-07-03 00:19:17 +0600

    Creating api for rtk query

--- redux-bootstrap-rtk/src/redux/api/baseApi.ts
+++ redux-bootstrap-rtk/src/redux/api/baseApi.ts
@@ -1,1 +1,11 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  endpoints: (builder) => ({
+    getTasks: builder.query({
+      query: () => "/tasks",
+    }),
+  }),
+});
