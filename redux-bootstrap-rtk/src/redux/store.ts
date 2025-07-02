9983ed098c1b0cf45126d328143a4e605292be93
Raufur Islam <raufurislam@gmail.com>
2025-07-03 05:59:07 +0600

    Random

--- redux-bootstrap-rtk/src/redux/store.ts
+++ redux-bootstrap-rtk/src/redux/store.ts
@@ -1,8 +1,15 @@
 // store.ts
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
+import taskReducer from "./features/task/taskSlice";
+import userReducer from "./features/task/userSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+    todo: taskReducer,
+    user: userReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
